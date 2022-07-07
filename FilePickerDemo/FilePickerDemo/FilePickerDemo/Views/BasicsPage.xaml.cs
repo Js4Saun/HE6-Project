267a461549ec932f4211df85cf7401b13da1530b
Joseph Mark Saunderson <80298035+Js4Saun@users.noreply.github.com>
2022-07-07 18:10:20 +0100

    Add files via upload

---
+++ FilePickerDemo/FilePickerDemo/FilePickerDemo/Views/BasicsPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using Plugin.XamarinFormsSaveOpenPDFPackage;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+namespace FilePickerDemo.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BasicsPage : ContentPage
+    {
+        public BasicsPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            // FOR PDF Files
+
+            var customFileType =
+                new FilePickerFileType(new Dictionary<DevicePlatform, IEnumerable<string>>
+                {
+                    { DevicePlatform.Android, new[] { "application/pdf" } },
+                });
+
+            var pickImage = await FilePicker.PickAsync(new PickOptions()
+            {
+                FileTypes = customFileType,
+                PickerTitle = "Pick a Page"
+            });
+
+            if (pickImage != null)
+            {
+                var stream = await pickImage.OpenReadAsync();
+
+                using (var memorystream = await FileSystem.OpenAppPackageFileAsync("Temperature Conversion.pdf"))
+                {
+                    using (var reader = new StreamReader(stream))
+                    {
+                        var fileContents = await reader.ReadToEndAsync();
+                    }
+                }
+
+                //using (var memorystream = new MemoryStream())
+                //{
+                //    await stream.CopyToAsync(memorystream);
+                //    await CrossXamarinFormsSaveOpenPDFPackage.Current.SaveAndView("myFile.pdf", "application/pdf", memorystream, PDFOpenContext.InApp);
+                //}
+            }
+        }
+    }
+}
---
+++ FilePickerDemo/FilePickerDemo/FilePickerDemo/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using FilePickerDemo.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace FilePickerDemo.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
